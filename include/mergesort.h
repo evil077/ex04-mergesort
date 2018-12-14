14a424f3f405a3833c62bde999d471f6bb07ccf9
brianda-plascencia <brianda_plascencia1@my.vcccd.edu>
2018-12-14 11:31:42 -0800

    turning

--- include/mergesort.h
+++ include/mergesort.h
@@ -11,6 +11,66 @@
 
 namespace edu { namespace vcccd { namespace vc { namespace csv15 {
 
+//Sherlock's Code
+
+                template<class Alpaca>
+                void merge(Alpaca lower[], Alpaca upper[], int64_t size) {
+                    std::cout << "lower = " << *lower << ", upper = " << *upper << ", size = " << size << std::endl;
+                    Alpaca *hold = new Alpaca[size];
+                    Alpaca *b = lower, *u = upper, *h = hold;
+                    // std::cout << "b = " << *b << ", u = " << *u << ", h = " << *h << std::endl;
+                    while(b < upper && u < upper + size/2 + size%2){
+                        if (*b < *u) {
+                            *h = *b;
+                            b++;
+                        } else {
+                            *h = *u;
+                            u++;
+                        }
+                        h++;
+                    }
+                    while (b < upper) {
+                        *h = *b;
+                        b++;
+                        h++;
+                    }
+                    while (u < upper + size/2 + size%2){
+                        *h = *u;
+                        u++;
+                        h++;
+                    }
+
+
+                    for (int64_t k = 0;k < size; k++) {
+                        std::cout<< hold[k] << " ";
+                        lower[k] = hold[k];
+                    }
+                    std::cout << std::endl;
+                    delete [] hold;
+                }
+
+
+                template<class Alpaca>
+
+    void mergesort(Alpaca array[], size_t size) {
+        std::cout << "array = " << array << ", size = " << size << std::endl;
+        if ( size > 2 ) {
+            mergesort(array, size/2);
+        //    std::cout << "array after mergesort 1 = " << array << ", size after mergesort 1 = " << size << std::endl;
+            mergesort(array + size/2, size/2 + (size % 2));
+        //    std::cout << "array after mergesort 2 = " << array << ", size after mergesort 2 = " << size << std::endl;
+            merge(array, array + size/2, size);
+        //    std::cout << "array after merge = " << array << ", size after merge = " << size << std::endl;
+        } else if (size == 2) {
+        //    std::cout << "If size == 2" << std::endl;
+            if (array[1] < array[0]) {
+        //        std::cout << "If array[1] < array[0]" << std::endl;
+                std::swap(array[0],array[1]);
+            }
+        }
+    }
+
+                /*
     template<class T>
 
     void mergesort(T array[], size_t size) {
@@ -54,7 +114,7 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
                           ", arraySize = " << arraySize << std::endl;
                 //the merge code for sorting the values and putting it into the temporary array
                 for (size_t k = 0; k < tempSize; k++) {
-                        //if right side value doesn't exist, skip the step
+                    //if right side value doesn't exist and left pointer still has values, copy left value
                     if (rightPtr >= arraySize && leftPtr < mid) {
                         tempArray[k] = array[leftPtr];
                         leftPtr++;
@@ -64,7 +124,7 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
                         std::cout << "the value that went into temp array is: " << tempArray[k] << std::endl << std::endl;
                         continue;
                     }
-                        //if left side value doesn't exist, skip the step
+                    //if left side value doesn't exist, skip the step
                     else if (leftPtr >= arraySize) {
                         leftPtr++;
                         //k--;
@@ -110,6 +170,7 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
             std::cout << "the value of m is " << array[m] << std::endl << std::endl;
         }
     }
+                 */
 }}}}
 
 #endif //EX04_MERGESORT_MERGESORT_H
