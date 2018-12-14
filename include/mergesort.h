105d3105e983cf01eef45d5fbd331457a639e37e
brianda-plascencia <brianda_plascencia1@my.vcccd.edu>
2018-12-14 10:44:32 -0800

    turning

--- include/mergesort.h
+++ include/mergesort.h
@@ -41,7 +41,7 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
             int x = 0;
             std::cout << "i = " << i << ", x = " << x << ", y = " << y << ", arraySize = " <<
                       arraySize << std::endl;
-            for (size_t j = 0; j < arraySize + 2; (j += pow(2, i)), x++) {
+            for (size_t j = 0; j < arraySize; (j += pow(2, i)), x++) {
                 size_t mid = (j + pow(2, i)) / 2 + (x * pow(2, y));
                 size_t tempSize = (pow(2, i));
                 std::cout << "before alloc: "<< tempSize << std::endl;
@@ -54,46 +54,38 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
                           ", arraySize = " << arraySize << std::endl;
                 //the merge code for sorting the values and putting it into the temporary array
                 for (size_t k = 0; k < tempSize; k++) {
-                    // when the left side is out of values, copy values from right to the temporary array
-                    if (leftPtr >= mid) {
-                        tempArray[k] = array[rightPtr];
-                        rightPtr++;
-                        std::cout << "rightPtr = " << rightPtr << ", mid = " << mid << std::endl<< std::endl;
-                        std::cout << "the value that went into temp array is: " << tempArray[k] << std::endl << std::endl;
-                    }
-                        // when the right side is out of values, copy values from left to the temporary array
-                    else if (rightPtr >= tempSize) {
+                        //if right side value doesn't exist, skip the step
+                    if (rightPtr >= arraySize && leftPtr < mid) {
                         tempArray[k] = array[leftPtr];
                         leftPtr++;
-                        std::cout << "leftPtr = " << leftPtr << ", tempSize = " << tempSize << std::endl << std::endl;
-                        std::cout << "the value that went into temp array is: " << tempArray[k] << std::endl << std::endl;
-                    }
-                        //if right side value doesn't exist, skip the step
-                    else if (rightPtr >= arraySize) {
                         rightPtr++;
-                        k--;
-                        std::cout << "rightPtr = " << rightPtr << ", arraySize = " << arraySize << std::endl << std::endl;
-                        std::cout << "the value that went into temp array is: nothing" << std::endl << std::endl;
+                        std::cout << "rightPtr = " << rightPtr << ", leftPtr = " << leftPtr << ", arraySize = " <<
+                            arraySize << std::endl << std::endl;
+                        std::cout << "the value that went into temp array is: " << tempArray[k] << std::endl << std::endl;
+                        continue;
                     }
                         //if left side value doesn't exist, skip the step
                     else if (leftPtr >= arraySize) {
                         leftPtr++;
-                        k--;
+                        //k--;
                         std::cout << "leftPtr = " << leftPtr << ", arraySize = " << arraySize << std::endl << std::endl;
                         std::cout << "the value that went into temp array is: nothing" << std::endl << std::endl;
+                        continue;
                     }
-/*                    // when the left side is out of values, copy values from right to the temporary array
+                    // when the left side is out of values, copy values from right to the temporary array
                     else if(leftPtr >= mid){
                         tempArray[k] = array[rightPtr];
                         rightPtr++;
-                        std::cout << "rightPtr = " << rightPtr << ", mid = " << mid << std::endl;
+                        std::cout << "rightPtr = " << rightPtr << ", mid = " << mid << std::endl << std::endl;
+                        std::cout << "the value that went into temp array is: " << tempArray[k] << std::endl << std::endl;
                     }
                     // when the right side is out of values, copy values from left to the temporary array
-                    else if(rightPtr >= tempSize){
+                    else if(rightPtr >= tempSize) {
                         tempArray[k] = array[leftPtr];
                         leftPtr++;
-                        std::cout << "leftPtr = " << leftPtr << ", tempSize = " << tempSize << std::endl;
-                    }*/
+                        std::cout << "leftPtr = " << leftPtr << ", tempSize = " << tempSize << std::endl << std::endl;
+                        std::cout << "the value that went into temp array is: " << tempArray[k] << std::endl << std::endl;
+                }
                         //if the left value is greater than the right value, copy the left value to the temporary array
                     else if (array[leftPtr] < array[rightPtr]) {
                         tempArray[k] = array[leftPtr];
